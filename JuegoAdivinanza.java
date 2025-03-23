af8e5006ade7d1a9bd1d79f39b0f4019940b7bd1
Diego Garcia Mendoza <diegosg826@gmail.com>
2025-03-23 02:43:18 -0600

    Implementacion funcion principal
    Implementada la estructura principal del programa

---
+++ JuegoAdivinanza.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class JuegoAdivinanza{
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num=0; //Numero ingresado por el usuario
+        int intentos=0; //Numero de intentos
+
+        System.out.println("Juego de Adivinanza");
+        System.out.println("Tendras (NUMERO DE INTENTOS) intentos para adivinar un numero de entre 1 y 100\n");
+
+
+        while(intentos<3){ //REEMPLAZAR EL 3 POR UNA VARIABLE DE LIMITE DE INTENTOS
+            System.out.print("Ingresa un numero: ");
+            num=scanner.nextInt();
+
+            if(num<1 || num>100){
+                System.out.println("El numero ingresado no esta en el rango de 1 a 100");
+                continue; //NO SE CUENTA COMO UN INTENTO
+            }
+
+            
+
+            //IMPLEMENTAR LA LOGICA PARA ADIVINAR EL NUMERO
+
+
+
+            //IMPLEMENTAR LAS PISTAS
+
+
+
+
+            //IMPLEMENTAR EL LIMITE DE INTENTOS Y EL AUMENTO DE INTENTOS HECHOS
+
+
+
+
+
+        }
+        
+        scanner.close();
+    }
+}
