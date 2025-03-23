27f9223752140fe72f59bdeb8d070f734651d9e2
Diego Garcia Mendoza <diegosg826@gmail.com>
2025-03-23 02:57:01 -0600

    Implementacion del numero aleatorio
    Implementado la generacion del numero aleatorio

--- JuegoAdivinanza.java
+++ JuegoAdivinanza.java
@@ -1,14 +1,21 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class JuegoAdivinanza{
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+        int numeroAleatorio = rand.nextInt(100) + 1; //Numero aleatorio entre 1 y 100
+
         int num=0; //Numero ingresado por el usuario
-        int intentos=0; //Numero de intentos
+        int intentos=0; //Numero de intentos        
 
         System.out.println("Juego de Adivinanza");
         System.out.println("Tendras (NUMERO DE INTENTOS) intentos para adivinar un numero de entre 1 y 100\n");
+
+
+        System.out.println("El numero aleatorio es: "+numeroAleatorio); //ESTA LINEA SOLO ES PARA PRUEBAS, BORRAR EN LA VERSION FINAL
 
 
         while(intentos<3){ //REEMPLAZAR EL 3 POR UNA VARIABLE DE LIMITE DE INTENTOS
